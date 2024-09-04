54a61eda0be33d538eb153115f7ffed975ff102d
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-09-04 19:52:58 +0400

    Works ... experemental

--- HowUseSwiftDatainSwiftUIPreviews/ContentView.swift
+++ HowUseSwiftDatainSwiftUIPreviews/ContentView.swift
@@ -6,19 +6,83 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    @Query(sort: [SortDescriptor(\User.name, comparator: .localizedStandard)]) var users: [User]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(users) { user in
+                Text(user.name)
+            }
         }
-        .padding()
     }
 }
 
+struct EditingView: View {
+    @Environment(\.modelContext) var modelContext
+    @Bindable var user: User
+    
+    var body: some View {
+        Form {
+            TextField("Name", text: $user.name)
+        }
+    }
+}
+
+//#Preview {
+//    EditingView(user: <#User#>)
+//    ContentView(user: <#User#>)
+//}
+
 #Preview {
-    ContentView()
+    let config = ModelConfiguration(isStoredInMemoryOnly: true)
+    let container = try! ModelContainer(for: User.self, configurations: config)
+
+    for i in 1..<10 {
+        let user = User(name: "Example User \(i)")
+        container.mainContext.insert(user)
+    }
+
+    return ContentView()
+        .modelContainer(container)
+}
+
+#Preview {
+    let config = ModelConfiguration(isStoredInMemoryOnly: true)
+    let container = try! ModelContainer(for: User.self, configurations: config)
+    
+    let user = User(name: "Test User")
+    return EditingView(user: user)
+        .modelContainer(container)
+}
+
+
+@Model
+class User {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+}
+
+@MainActor
+class DataController {
+    static let previewContainer: ModelContainer = {
+        do {
+            let config = ModelConfiguration(isStoredInMemoryOnly: true)
+            let container = try ModelContainer(for: User.self, configurations: config)
+
+            for i in 1...9 {
+                let user = User(name: "Example User \(i)")
+                container.mainContext.insert(user)
+            }
+
+            return container
+        } catch {
+            fatalError("Failed to create model container for previewing: \(error.localizedDescription)")
+        }
+    }()
 }
