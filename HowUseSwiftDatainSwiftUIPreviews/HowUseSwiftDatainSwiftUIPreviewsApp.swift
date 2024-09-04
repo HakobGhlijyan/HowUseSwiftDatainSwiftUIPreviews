fec8ad06d1276f56798220f92a93f0b6b0b5e7b1
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-09-04 19:40:13 +0400

    Initial Commit

---
+++ HowUseSwiftDatainSwiftUIPreviews/HowUseSwiftDatainSwiftUIPreviewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowUseSwiftDatainSwiftUIPreviewsApp.swift
+//  HowUseSwiftDatainSwiftUIPreviews
+//
+//  Created by Hakob Ghlijyan on 04.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct HowUseSwiftDatainSwiftUIPreviewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
